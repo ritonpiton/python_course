fb2c090eecb1613c6329f51f880a7d406e45101b
Margarita Teaseman <margarita.aksiutina@ddgcorp.ru>
2024-05-22 17:57:16 +0300

    added logic

--- HW_1/task-2.py
+++ HW_1/task-2.py
@@ -10,15 +10,45 @@ def set_number():
 
     while True:
         if is_valid_number_input(number, min_value, max_value):
-            return int(number)
+            return number
         else:
             number = input("Wrong input, please try again: ")
 
 
+def generate_text(number):
+    units_words = ["", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"]
+    teens_words = ["десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать",
+             "семнадцать", "восемнадцать", "девятнадцать"]
+    tens_words = ["", "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят",
+            "девяносто"]
+    hundreds_words = ["", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"]
+
+    words=[]
+
+    # разбиваем на сотни
+    hundred = int(number) // 100
+    # разбиваем на десятки
+    ten = (int(number) % 100) // 10
+    # разбиваем на единицы
+    unit = int(number) % 10
+
+    if hundred > 0:
+        words.append(hundreds_words[hundred])
+
+    if ten == 1:
+        words.append(teens_words[unit])
+    else:
+        if ten > 0:
+            words.append(tens_words[ten])
+        if unit > 0:
+            words.append(units_words[unit])
+    return " ".join(words)
+
+
 def main():
     number = set_number()
-
-    print(number)
+    result = generate_text(number)
+    print(result)
 
 
 main()
