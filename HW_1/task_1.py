1b484cfe6b941596d360747fdc5b5796febaf950
Margarita Teaseman <margarita.aksiutina@ddgcorp.ru>
2024-05-22 21:35:22 +0300

    added logic for task 1

---
+++ HW_1/task_1.py
@@ -0,0 +1,29 @@
+def is_valid_number_input(n):
+    return n.isdigit() and bool(int(n))
+
+
+def set_number():
+    number = input(f"Enter a number: ")
+
+    while True:
+        if is_valid_number_input(number):
+            return int(number)
+        else:
+            number = input("Wrong input, please try again: ")
+
+
+def init_triangle(number):
+    spaces = ""
+    stars = ""
+    for i in range(number):
+        spaces = " " * (number - i - 1)
+        stars = "*" * (2 * i + 1)
+        print(spaces + stars)
+
+
+def main():
+    number = set_number()
+    init_triangle(number)
+
+
+main()
